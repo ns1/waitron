2627de880789c2382b7776c49ef28f9c62142c6a
Johan Haals <johan.haals@gmail.com>
2015-09-25 13:15:34 -0400

    Machine test

---
+++ machine_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func TestmachineDefinition(t *testing.T) {
+	m, err := machineDefinition("example.com", "machines")
+	if err != nil {
+		t.Errorf("Unable to load test machine definition")
+	}
+	if m.Hostname != "my-service.example.com" {
+		t.Errorf("expected hostname: my-service.example.com")
+	}
+	if m.ShortName != "my-service" {
+		t.Errorf("invalid shortname")
+	}
+}
+
+func TestRenderTemplate(t *testing.T) {
+	config, _ := loadConfig("config.yaml")
+	m, _ := machineDefinition("example.com", "machines")
+
+	template, err := m.renderTemplate("pxe.j2", config)
+	if err != nil {
+		t.Errorf("failed to render template")
+	}
+
+	expected := "example.com"
+	if !strings.Contains(template, expected) {
+		t.Errorf(fmt.Sprintf("Template does not contain '%s'", expected))
+	}
+}
