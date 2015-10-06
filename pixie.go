248025565f203f6624301c1184d082cc8664d95f
Xavier Naveira <xavier.naveira@ooyala.com>
2015-10-06 15:48:25 +0200

    Removed some comments

--- pixie.go
+++ pixie.go
@@ -1,9 +1,5 @@
 package main
 
-// import (
-// 	"log"
-// )
-
 type Pixie struct {
 	Kernel string `json:"kernel"`
 	Initrd string `json:"initrd"`
@@ -12,8 +8,6 @@ type Pixie struct {
 func pixieInit(config Config) Pixie {
 	var p Pixie
 
-	//p.Kernel = "http://aptly.videoplaza.org/ubuntu/dists/trusty/main/installer-amd64/current/images/cdrom/vmlinuz"
-	//p.Initrd = "http://aptly.videoplaza.org/ubuntu/dists/trusty/main/installer-amd64/current/images/cdrom/initrd.gz"
 	p.Kernel = config.ImageURL + config.Kernel
 	p.Initrd = config.ImageURL + config.Initrd
 
