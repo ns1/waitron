2b971b7c1c339734209c574b66e9722280d8ab4c
Xavier Naveira <xavier.naveira@ooyala.com>
2015-10-07 09:13:55 +0200

    initrd as a list

--- pixie.go
+++ pixie.go
@@ -1,15 +1,15 @@
 package main
 
 type Pixie struct {
-	Kernel string `json:"kernel"`
-	Initrd string `json:"initrd"`
+	Kernel string   `json:"kernel"`
+	Initrd []string `json:"initrd"`
 }
 
 func pixieInit(config Config) Pixie {
 	var p Pixie
 
 	p.Kernel = config.ImageURL + config.Kernel
-	p.Initrd = config.ImageURL + config.Initrd
+	p.Initrd = []string{config.ImageURL + config.Initrd}
 
 	return p
 }
